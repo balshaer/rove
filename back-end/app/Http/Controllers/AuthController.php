dd1c919366e2d016b047faff2694c95cdffefe2c
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-01-31 14:01:59 +0200

    create: access token functionality

--- back-end/app/Http/Controllers/AuthController.php
+++ back-end/app/Http/Controllers/AuthController.php
@@ -8,6 +8,9 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Laravel\Passport\RefreshTokenRepository;
+use Laravel\Passport\TokenRepository;
 
 class AuthController extends Controller
 {
@@ -19,12 +22,14 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->passowrd)
+            'password' => Hash::make($request->password)
         ]);
         $token = $user->createToken('token')->accessToken;
+        $refreshToken = $user->createToken('authTokenRefresh')->accessToken;
         return response()->json([
             'user' => $user,
-            'token' => $token
+            'token' => $token,
+
         ], 200);
     }
 
@@ -43,7 +48,15 @@ class AuthController extends Controller
 
         return response()->json([
             'user' => $user,
-            'token' => $token
+            'token' => $token,
+
         ], 200);
+    }
+
+    // Logout Method
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json(['message' => 'Successfully logged out'], 200);
     }
 }
