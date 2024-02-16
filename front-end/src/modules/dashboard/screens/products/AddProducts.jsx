86e1ccdac9804b6ef24a98dfcd8b39cf4ed971e6
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-02-16 15:35:57 +0200

    feat: Implement customizable product creation fields on add products page

--- front-end/src/modules/dashboard/screens/products/AddProducts.jsx
+++ front-end/src/modules/dashboard/screens/products/AddProducts.jsx
@@ -1,8 +1,63 @@
 import AnimatedComponent from "@/components/custom/animation/AnimatedComponent";
-
+import { useState } from "react";
+import Select from "react-select";
+import { Input } from "@/components/ui/input";
+import Button from "@/components/custom/buttons/Button";
 
 export default function AddProducts() {
+  const [category, setCategory] = useState(null);
+
+  const options = [
+    { value: "", label: "User" },
+    { value: "", label: "Writer" },
+    { value: "", label: "Admin" },
+    { value: "", label: "Product Manger" },
+  ];
   return (
-    <AnimatedComponent>AddProducts</AnimatedComponent>
-  )
+    <AnimatedComponent>
+      <div className="flex gap-4 flex-col">
+        <div>
+          <Select
+            onChange={(selectedOption) => setCategory(selectedOption)}
+            name="role"
+            id="role"
+            value={options.label}
+            options={options}
+          />
+        </div>
+
+        <div>
+          <Input className="h-max w-full" type="text" placeholder="Title" />
+        </div>
+
+        <div>
+          <Input
+            className="h-max w-full"
+            type="text"
+            placeholder="Description"
+          />
+        </div>
+
+        <div>
+          <Input className="h-max w-full" type="text" placeholder="Price" />
+        </div>
+
+        <div>
+          <Input className="h-max w-full" type="text" placeholder="Discount" />
+        </div>
+
+        <div>
+          <Input className="h-max w-full" type="text" placeholder="About" />
+        </div>
+
+        <div>
+          <Input className="h-max w-full" type="file" placeholder="Image" />
+        </div>
+
+        <div>
+          <Button text="Submit" />
+        </div>
+      </div>
+    </AnimatedComponent>
+  );
 }
--- front-end/src/modules/main/screens/Products.jsx
+++ front-end/src/modules/main/screens/Products.jsx
@@ -1,3 +1,14 @@
+import Footer from "@/components/global/website/footer/Footer";
+import Navbar from "@/components/global/website/navbar/Navbar";
+
 export default function Products() {
-  return <div>sad</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <div></div>
+
+      <Footer />
+    </div>
+  );
 }
